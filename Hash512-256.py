f892a39cb5d517486a4cb7763dcf31873456ccd4
Aldar Cabrelles <33228351+acabrelles@users.noreply.github.com>
2023-04-20 10:39:39 +0200

    Update Hash512-256.py

--- Hash512-256.py
+++ Hash512-256.py
@@ -1,20 +1,22 @@
 import pandas as pd
 from Crypto.Hash import SHA512
 
-#These are examples of the variables to be used. You can use this sytem instead of the input system below.
-#importpath = '/Users/aldar/Documents/CRG-EGA/EuCanImage/Hashing/Import/Test.csv'
-#secretkey = 'SgkZFJIlWeTJ7rZaeJq8jhRrWqknQSKfOCF99cRMIEg07aBPMkTivFjtn7XE83pu'
-#columnid = 'PatientID'
-#exportpath = '/Users/aldar/Documents/CRG-EGA/EuCanImage/Hashing/Export/TestHash.csv'
+These are examples of the variables to be used. You can use this sytem instead of the input system below.
+importpath = '/Users/aldar/Documents/CRG-EGA/EuCanImage/Hashing/Import/Test.csv'
+secretkey = 'SgkZFJIlWeTJ7rZaeJq8jhRrWqknQSKfOCF99cRMIEg07aBPMkTivFjtn7XE83pu'
+columnid = 'PatientID (0010,0020)'
+hashid = 'CMRAD Hash'
+exportpath = '/Users/aldar/Documents/CRG-EGA/EuCanImage/Hashing/Export/TestHash.csv'
 
 #This section can be deleted/hidden in comments if you prefer using the variables above directly instead
-importpath = input('Enter the absolute path of the file containing the IDs to be hashed: ')
-secretkey = input('Enter the secret key to be used for hashing: ')
-columnid = input('Enter the name of the column containing the IDs to be hashed: ')
-exportpath = input('Enter the absolute path of the file to export the new file with the Hashed IDs: ')
+#importpath = input('Enter the absolute path of the file containing the IDs to be hashed: ')
+#secretkey = input('Enter the secret key to be used for hashing: ')
+#columnid = input('Enter the name of the column containing the IDs to be hashed: ')
+#hashid = input('Enter the name of the column containing the hashed IDs: ')
+#exportpath = input('Enter the absolute path of the file to export the new file with the Hashed IDs: ')
 
 #Convert column of patient IDs into a list
-df = pd.read_csv(importpath,sep=',')
+df = pd.read_csv(importpath,sep=';')
 patientidlist = df[columnid].values.tolist()
 
 #Create new list of hashed IDs
@@ -28,7 +30,7 @@ for index in range(len(df)):
     hashlist.append(digest)
 
 #Add new column of hashed IDs into the data frame
-df['Hash-ID'] = hashlist
+df[hashid] = hashlist
 
 #Export modified data frame into a new csv
 df.to_csv(exportpath,index=False)
